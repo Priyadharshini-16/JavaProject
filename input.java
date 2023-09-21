dbb0fd90fbbc725e36e2f77d62165f010bae0561
PRIYADHARSHINI <145644895+Priyadharshini-16@users.noreply.github.com>
2023-09-21 15:05:30 +0530

    Update input.java

--- input.java
+++ input.java
@@ -4,17 +4,17 @@ public class input
 public static void main(String agrs[])
 {
 int card,pin;
-String user;
+String username;
 
 Scanner obj=new Scanner(System.in);
 System.out.print("Enter the username:");
-user=obj.nextLine();
+username=obj.nextLine();
 System.out.println("Enter Card no:");
 card=obj.nextInt();
 System.out.println("Enter your card pin:");
 pin=obj.nextInt();
 
-if((card == 12345 && pin == 0000) ||(user=="user"))
+if((card == 12345 && pin == 0000) ||(username=="user"))
 {
 System.out.println("Valid");
 }
